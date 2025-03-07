7f03ef35fe895d48bd9d4d9b30343dbbd3c0da5b
Jherryett Salvador <jherryett@gmail.com>
2025-03-07 12:06:41 -0300

    Realizando o desafio de POO de .NET

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone Telefone1 = new Iphone("1254", "Iphone", "123456789", 64);
+Nokia Telefone2 = new Nokia("5689", "Nokia", "987654321", 32);
+
+Telefone1.InstalarAplicativo("Linkedin");
+Telefone2.InstalarAplicativo("WhatsApp");
